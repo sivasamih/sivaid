224ef56b7b85e36e9a7b3679264769c47c78cfa6
Samihan <74051958+sivasamih@users.noreply.github.com>
2022-06-11 11:11:24 +0530

    Samihan updates

--- src/productDetail.js
+++ src/productDetail.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(".category-products-list").hide();
     let productList = [];
-    productList=[{
+    productList = [{
         "image": "img/products/productthumbnail/thumbnail.jpg",
         "name": "Sample product",
         "desc": "This is a sample product, which is for dummy showcase",
@@ -10,7 +10,7 @@ $(document).ready(function () {
         "link": "#",
         "isNew": ""
     }]
-    let notPresent=false;
+    let notPresent = false;
     let breadcrumbTitle = pageSubHeading = pageBanner = detailHtml = catDetail = "";
     try {
         const urlParams = new URLSearchParams(window.location.search);
@@ -26,14 +26,13 @@ $(document).ready(function () {
                     $("meta[name='keywords']").attr('content', 'example keywords');
                     $("meta[name='description']").attr('content', 'example description');
                     pageBanner = "img/products/productpage/banner/banner-chemical-labelling.jpg";
-                    pageSubHeading = "Robust Labels for Chemicals";
+                    pageSubHeading = "Robust labels for hazardous chemicals";
                     detailHtml = `
                     <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                    SIVA is a responsible supplier of labels for the manufacturing sectors that require labelling for hazardous products. Our knowledge of the IMDG and GHS regulations for the identification of these products allows us to offer guidance on best fit solutions for your labelling needs. Our manufacturing sites use only the highest quality materials to ensure that you are labelling up you're packaging in a responsible manner. Printing technology within this sector is rapidly changing as the shift from thermal printers to full-colour solutions rapidly increases. At SIVA, we fully understand the challenges involved and are happy to offer guidance on best fit solutions, that will allow your business to run efficiently.
                     </p>
                    
             `;
-
                     break;
                 case "Horticulture Tags":
                     $("meta[name='keywords']").attr('content', 'example keywords');
@@ -60,14 +59,14 @@ $(document).ready(function () {
             `;
                     break;
                 default:
-                    notPresent=true;
+                    notPresent = true;
                     break;
             }
-        }else{
-            notPresent=true;  
+        } else {
+            notPresent = true;
         }
 
-       
+
 
         if (notPresent === false) {
             $("#breadcrumbTitle").text(breadcrumbTitle);
@@ -77,45 +76,45 @@ $(document).ready(function () {
             $("#pageSubHeading").html(pageSubHeading);
             $("#detailHtml").html(detailHtml);
 
+
+
+
+            /*
+                        var tr = "";
+                        for (var j = 0; j < productList.length; j++) {
+                            tr = tr + "<tr><td data-label='PRODUCT' class='align-center mobile-align-right'> <a href='" + productList[j]['link'] + "'><img class='card' src='" + productList[j]['image'] + "'/> </a></td> <td data-label='PRODUCT NAME'> <h2 class='table-h2'> <a href='" + productList[j]['link'] + "'>" + productList[j]['name'] + "</a> " + productList[j]['isNew'] + "</h2> <p>" + productList[j]['desc'] + "</p> </td> <td  data-label='DIMENSIONS' class='align-right'><p> " + productList[j]['dimensions'] + " </p></td> <td data-label='READ RANGE' class='align-right'><p> " + productList[j]['read-range'] + " </p></td>   </tr>";
             
-
-
-/*
-            var tr = "";
-            for (var j = 0; j < productList.length; j++) {
-                tr = tr + "<tr><td data-label='PRODUCT' class='align-center mobile-align-right'> <a href='" + productList[j]['link'] + "'><img class='card' src='" + productList[j]['image'] + "'/> </a></td> <td data-label='PRODUCT NAME'> <h2 class='table-h2'> <a href='" + productList[j]['link'] + "'>" + productList[j]['name'] + "</a> " + productList[j]['isNew'] + "</h2> <p>" + productList[j]['desc'] + "</p> </td> <td  data-label='DIMENSIONS' class='align-right'><p> " + productList[j]['dimensions'] + " </p></td> <td data-label='READ RANGE' class='align-right'><p> " + productList[j]['read-range'] + " </p></td>   </tr>";
-
-            }
+                        }
+                        
+                        catDetail = "<h1 class='align-center theme-color'><b>PRODUCT LIST</b></h1><br /><div id='product_list_c' class='category-products-list'>" +
+                            "<div class='container product-details'>" +
+                            "<div class='card'>" +
+                            "<table id='product_table_c'>" +
+                            "<thead>" +
+                            "<tr>" +
+                            "<th class='product-col-width'>PRODUCT</th>" +
+                            "<th class='product-name-col-width'>PRODUCT NAME</th>" +
+                            "<th class='dimension-col-width'>DIMENSIONS</th>" +
+                            "<th class='read-range-col-width'>READ RANGE</th>" +
             
-            catDetail = "<h1 class='align-center theme-color'><b>PRODUCT LIST</b></h1><br /><div id='product_list_c' class='category-products-list'>" +
-                "<div class='container product-details'>" +
-                "<div class='card'>" +
-                "<table id='product_table_c'>" +
-                "<thead>" +
-                "<tr>" +
-                "<th class='product-col-width'>PRODUCT</th>" +
-                "<th class='product-name-col-width'>PRODUCT NAME</th>" +
-                "<th class='dimension-col-width'>DIMENSIONS</th>" +
-                "<th class='read-range-col-width'>READ RANGE</th>" +
-
-                "</tr>" +
-                "</thead>" +
-                "<tbody>" + tr +
-                "</tbody>" +
-                "</table>" +
-                "</div>" +
-                "</div>" +
-                "</div>";
-*/
+                            "</tr>" +
+                            "</thead>" +
+                            "<tbody>" + tr +
+                            "</tbody>" +
+                            "</table>" +
+                            "</div>" +
+                            "</div>" +
+                            "</div>";
+            */
 
 
         } else {
-            catDetail=`
+            catDetail = `
               <h1 class='theme-color'>Oops!</h1> 
               <h5>Nothing Found.</h5>
             `;
-         }
-      
+        }
+
         $("#categoryDiv").html(catDetail);
 
 
