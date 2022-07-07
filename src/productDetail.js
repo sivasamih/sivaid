69e3a8708adaf924077ab4ba7decdc03dd18b170
Samihan <74051958+sivasamih@users.noreply.github.com>
2022-07-07 13:51:53 +0530

    Samihan updates

--- src/productDetail.js
+++ src/productDetail.js
@@ -1,15 +1,6 @@
 $(document).ready(function () {
     $(".category-products-list").hide();
-    let productList = [];
-    productList = [{
-        "image": "img/products/productthumbnail/thumbnail.jpg",
-        "name": "Sample product",
-        "desc": "This is a sample product, which is for dummy showcase",
-        "dimensions": "-",
-        "read-range": "-",
-        "link": "#",
-        "isNew": ""
-    }]
+    let productCategory = [];
     let notPresent = false;
     let breadcrumbTitle = pageSubHeading = pageBanner = detailHtml = catDetail = "";
     try {
@@ -26,10 +17,10 @@ $(document).ready(function () {
                     $("meta[name='keywords']").attr('content', 'example keywords');
                     $("meta[name='description']").attr('content', 'example description');
                     pageBanner = "img/products/productpage/banner/banner-chemical-labelling.jpg";
-                    pageSubHeading = "Heat Resistant Robust tags for Steel, Metals & Construction";
+                    pageSubHeading = "Robust labels for hazardous chemicals";
                     detailHtml = `
                         <p>
-                        Siva Group has developed a range of durable tags and labels to meet the demands of the Steel, Metals and Construction industries. We understand the demands placed on labelling in these heavy industries, we aim to match the tag or label to best suit your product or packaging needs. Our manufacturing sites use only the highest quality materials, we are ISO9001:2015 certified across 4 key sites. Our team has been working in these markets for over 25 years.
+                        SIVA is a responsible supplier of labels for the manufacturing sectors that require labelling for hazardous products. Our knowledge of the IMDG and GHS regulations for the identification of these products allows us to offer guidance on best fit solutions for your labelling needs. Our manufacturing sites use only the highest quality materials to ensure that you are labelling the packaging in a responsible manner. Printing technology within this sector is rapidly changing as the shift from thermal printers to full-colour solutions rapidly increases. At SIVA, we fully understand the challenges involved and are happy to offer guidance on best fit solutions that will allow your business to run efficiently.
                         </p>
                    
             `;
@@ -38,23 +29,51 @@ $(document).ready(function () {
                     $("meta[name='keywords']").attr('content', 'example keywords');
                     $("meta[name='description']").attr('content', 'example description');
                     pageBanner = "img/products/productpage/banner/banner_horticulture.jpg";
-                    pageSubHeading = "RFID in the agriculture and horticulture sector";
+                    pageSubHeading = "Tags & Labels for Growers, Nurseries and Garden Centres";
                     detailHtml = `
            
             <p>
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            Siva Group manufactures and supplies a range of durable tags and labels to meet the demands of Growers, Nurseries and Garden Centre's. We use only the highest quality materials and are ISO9001:2015 certified across 5 key sites.
             </p>
            
             `;
+                    productCategory = [
+                        {
+                            "name": "Loop Lock Tags",
+                            "desc": "Loop Locks, Self-Ties, Tie-Wraps, Wrap Around labels… whatever you call them we make them. Ideal for identifying your plants through their journey from greenhouse to garden or home.",
+                            products:[]
+                        },
+                        {
+                            "name": "Stick In Tags",
+                            "desc": "Stick In's, Pot Pointer's, Pointed-End Labels… again an array of descriptions for a tag that can be inserted into the compost of your pot or tray. They help to identify your plant and share important information to your customer. ",
+                            products:[]
+                        },
+                        {
+                            "name": "Sticky Labels",
+                            "desc": "Labelling pots or trays that are coming into contact with composts, watering and weathering conditions can be challenging. Our self-adhesive “sticky” labels are available in a range of sizes to suit your needs and enable you to add your plant data (passport number, barcodes, descriptions, care instructions, pricing) knowing it will still be legible when the pot arrives with the customer.",
+                            products:[]
+                        },
+                        {
+                            "name": "Bed Cards",
+                            "desc": "A great way to present your stock in a colourful and informative style to help you sell more products. Bed cards can be printed by colour desktop laser printers (OKI, Xerox, Canon, Lexmark etc.). Siva can supply blank media in tag or self-adhesive options to suit your needs.",
+                            products:[]
+                        },
+                        {
+                            "name": "Customised Tags & Labels",
+                            "desc": "For a specific size/shape or a printed tag or label that is not available from our stock, we aim to work with you on a date to suit your needs. We can supply sheets, rolls or fan-folded tags dependent on your requirement. We will help plan upcoming deliveries and ensure you are not in a position where you cannot print a label or tag.",
+                            products:[]
+                        },
+                         
+                    ];
                     break;
                 case "Tags for Steel and Construction":
                     $("meta[name='keywords']").attr('content', 'example keywords');
                     $("meta[name='description']").attr('content', 'example description');
                     pageBanner = "img/products/productpage/banner/banner_steel.jpg";
-                    pageSubHeading = "Robust Labels for Steel";
+                    pageSubHeading = "Heat Resistant Robust tags for Steel, Metals & Construction";
                     detailHtml = `
                     <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                    Siva Group has developed a range of durable tags and labels to meet the demands of the Steel, Metals and Construction industries. As we understand the demands placed on labelling in these heavy industries, we aim to match the tag or label to best suit your product or packaging needs. Our manufacturing sites use only the highest quality materials, we are ISO9001:2015 certified across 4 key sites. Our team has been working in these markets for over 25 years.
                     </p>
             `;
                     break;
@@ -76,37 +95,82 @@ $(document).ready(function () {
             $("#pageSubHeading").html(pageSubHeading);
             $("#detailHtml").html(detailHtml);
 
+            ProductHtml= "";
+            for (let p = 0; p < productCategory.length; p++) {
+                let PRODUCT = "";
+                PRODUCT = `
+                        <div class='container product-details'>
+                            <div class='card'>
+                                <div class="wrapper">
+                                <div class="hoc container clear">
+                                    <div style="margin-left:20px;margin-right: 20px;">
+                                    <div class="group demo">
+                                        <div class="one_third first">
+                                        <img class="left-product-image" src="img/products/productpage/Steel.jpg"  alt="">
+                                        </div>
+                                        <div class="two_third">
+                                        <div style="margin-left: 20px;"> 
+                                        <p>
+                                            ${productCategory[p].desc}
+                                        </p>
+                                        <p>
+                                            <a class="blue-para-hover-green" href="#">Download datasheet</a>
+                                        </p>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    </div>
+                                    <div class="gap">&nbsp;</div>
+                                    <div style="margin-left:20px;margin-right: 20px;">
+                                    <div class="row align-center background-grey-lighten-4">
+                                        <div class="col-1">
+                                        <div class="gap">&nbsp;</div>
+                                        <span class="product-specs-heading">Type </span><br/><br/>
+                                        <span>info</span>
+                                        <div class="gap">&nbsp;</div>
+                                        </div>
+                                        <div class="col-1">
+                                        <div class="gap">&nbsp;</div>
+                                        <span class="product-specs-heading">Dimensions</span><br/><br/>
+                                        <span>info</span>
+                                        <div class="gap">&nbsp;</div>
+                                        </div>
+                                        <div class="col-1">
+                                        <div class="gap">&nbsp;</div> 
+                                        <span class="product-specs-heading">Sheet Size </span><br/><br/>
+                                        <span>info</span>
+                                        <div class="gap">&nbsp;</div>
+                                        </div>
+                                        <div class="col-1">
+                                        <div class="gap">&nbsp;</div>
+                                        <span class="product-specs-heading">Read Range </span><br/><br/>
+                                        <span>info</span>
+                                        <div class="gap">&nbsp;</div>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                </div>
+                `;
+                ProductHtml += `
+                <div id='c_${p}' class='category-title-div card' title='Click to View Category'>
+                    <div class='list-div-inner'>
+                        <h3 class='list-div-h3'>${productCategory[p].name}</h3>
+                    </div>
+                </div>
+    
+                <div id='product_list_c_${p}' class='category-products-list'>
+                    ${PRODUCT}
+                    <div class='category-div-margin'></div>
+                </div>
+               
+                `;
+               
+            }
 
-
-
-            /*
-                        var tr = "";
-                        for (var j = 0; j < productList.length; j++) {
-                            tr = tr + "<tr><td data-label='PRODUCT' class='align-center mobile-align-right'> <a href='" + productList[j]['link'] + "'><img class='card' src='" + productList[j]['image'] + "'/> </a></td> <td data-label='PRODUCT NAME'> <h2 class='table-h2'> <a href='" + productList[j]['link'] + "'>" + productList[j]['name'] + "</a> " + productList[j]['isNew'] + "</h2> <p>" + productList[j]['desc'] + "</p> </td> <td  data-label='DIMENSIONS' class='align-right'><p> " + productList[j]['dimensions'] + " </p></td> <td data-label='READ RANGE' class='align-right'><p> " + productList[j]['read-range'] + " </p></td>   </tr>";
-            
-                        }
-                        
-                        catDetail = "<h1 class='align-center theme-color'><b>PRODUCT LIST</b></h1><br /><div id='product_list_c' class='category-products-list'>" +
-                            "<div class='container product-details'>" +
-                            "<div class='card'>" +
-                            "<table id='product_table_c'>" +
-                            "<thead>" +
-                            "<tr>" +
-                            "<th class='product-col-width'>PRODUCT</th>" +
-                            "<th class='product-name-col-width'>PRODUCT NAME</th>" +
-                            "<th class='dimension-col-width'>DIMENSIONS</th>" +
-                            "<th class='read-range-col-width'>READ RANGE</th>" +
-            
-                            "</tr>" +
-                            "</thead>" +
-                            "<tbody>" + tr +
-                            "</tbody>" +
-                            "</table>" +
-                            "</div>" +
-                            "</div>" +
-                            "</div>";
-            */
-
+            $("#ProductHtml").html(ProductHtml);
 
         } else {
             catDetail = `
